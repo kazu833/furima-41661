8c9aa6d9f0a089f333f8f075ee8dd6c548d54fa0
kazu833 <tomsfashion833@gmail.com>
2025-02-09 22:35:07 +0900

    FactoryBot設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    email                 {'test@com'}
-    password              {'test11'}
-    password_confirmation {password}
-    nickname              {'test'}
-    last_name             {'てすと'}
-    first_name            {'てすお'}
-    last_name_kana        {'テスト'}
-    first_name_kana       {'テスオ'}
-    birthday              {'2000/10/10'}
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password } 
+    nickname              { Faker::Name.initials(number:5) }
+    last_name             { Faker::Name.last_name }
+    first_name            { Faker::Name.first_name }
+    last_name_kana        { Gimei.last.katakana }
+    first_name_kana       { Gimei.first.katakana }
+    birthday  {Faker::Date.birthday(min_age: 18, max_age: 65).strftime('%Y/%m/%d')}
   end
 end
