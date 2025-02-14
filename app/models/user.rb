0cb17acf7deb1e0e1548b74bf0f1d88a8746e299
kazu833 <tomsfashion833@gmail.com>
2025-02-14 15:39:09 +0900

    プルリクエスト　修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字で入力してください' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'は全角(カタカナ)で入力してください' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'は全角(カタカナ)で入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角(カタカナ)で入力してください' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角(カタカナ)で入力してください' }
   validates :birthday, presence: true
   validates :nickname, presence: true
 
