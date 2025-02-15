cbd5659db9b71e559f1b43d3012d41f0d027d36b
kazu833 <tomsfashion833@gmail.com>
2025-02-15 21:09:36 +0900

    itemコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,19 @@
 class ItemsController < ApplicationController
-  
+
   def index
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create(item_params)
+    redirect_to root_path
   end
   
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_burden_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
