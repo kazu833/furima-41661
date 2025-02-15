3b08131291f868f35bab4acee66694e5e99fe525
kazu833 <tomsfashion833@gmail.com>
2025-02-15 21:27:30 +0900

    Active Storageの導入

--- Gemfile
+++ Gemfile
@@ -83,5 +83,6 @@ group :production do
 end
 
 gem 'devise'
-
 gem 'gimei'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,6 @@ class ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_burden_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :shipping_cost_burden_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
