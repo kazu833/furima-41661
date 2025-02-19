25f826b00d45114adce10a7e4cc290151fb86e62
kazu833 <tomsfashion833@gmail.com>
2025-02-19 15:13:25 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  validates :item_name, null: false
+  validates :description, null: false
+  validates :category_id, null: false
+  validates :condition_id, null: false
+  validates :shipping_cost_burden_id, null: false
+  validates :prefecture_id, null: false
+  validates :shipping_day_id, null: false
+  validates :price, null: false
+  validates :user, null: false, foreign_key: true
+  
 end
