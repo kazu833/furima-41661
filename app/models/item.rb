bd9fc23aabf10cfb31ae44e6414c9bd3eec52922
kazu833 <tomsfashion833@gmail.com>
2025-02-19 20:38:39 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,11 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_cost_burden_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_burden_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true
   validates :user, presence: true
   validates :image, presence: true
