ba5194a4706c569f4c262e381ee5054f4e8e8553
kazu833 <tomsfashion833@gmail.com>
2025-02-20 01:12:38 +0900

    価格表示機能　修正

--- app/javascript/items.js
+++ app/javascript/items.js
@@ -1,22 +1,21 @@
-
 window.addEventListener('turbo:load', function(){
 
   const priceInput = document.getElementById('item-price');
-  const addTaxPrice = document.getElementById('add-tax-price');
+  priceInput.addEventListener("input", () => {
+  const inputValue = parseFloat(priceInput.value);
+  
+  if (inputValue >= 300 && inputValue <= 9999999) {
+  const addTaxDom = document.getElementById('add-tax-price');
+  const tax = Math.floor(inputValue * 0.10);
+  addTaxDom.innerHTML = tax;
+  
   const profit = document.getElementById('profit');
-
-  priceInput.addEventListener('input', () => {
-    const price = priceInput.value; 
-
-    if (price >= 300 && price <= 9999999) {
-       const fee = Math.floor(price * 0.10);
-      const gains = price - fee;
-
-      addTaxPrice.textContent = fee.toLocaleString();
-      profit.textContent = gains.toLocaleString();
-    } else {
-      addTaxPrice.textContent = '0';
-      profit.textContent = '0';
-    }
+  profit.textContent = Math.floor(inputValue - tax);
+  } else {
+  addTaxDom.textContent = '0';
+  profit.textContent = '0';
+      }
   });
-});
+  });
+  window.addEventListener("turbo:load", price);
+  window.addEventListener("turbo:render", price);
