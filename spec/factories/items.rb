629a01ae139f1bbe6fd455ca5a0fa3b5d2ea9ecb
kazu833 <tomsfashion833@gmail.com>
2025-02-22 01:01:13 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name                   { Faker::Name.initials(number: 5) }
+    description                 { Faker::Lorem.paragraph_by_chars(number: 1000, supplemental: false) }
+    category_id                 { (2..11).to_a.sample }
+    condition_id                { (2..7).to_a.sample }
+    shipping_cost_burden_id     { (2..3).to_a.sample }
+    prefecture_id               { (2..48).to_a.sample }
+    shipping_day_id             { (2..4).to_a.sample }
+    price                       { Faker::Number.between(from: 300, to: 9999999) }
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,112 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user: @user)  
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できる場合' do
+      it 'すべての項目が適切に入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品できない場合' do
+      it '商品名が空では出品できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Item name can't be blank"
+      end
+
+      it '商品名が40字以上では出品できない' do
+        @item.item_name = 'a' * 41
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Item name is too long (maximum is 40 characters)"
+      end
+
+      it '商品説明が空では出品できない' do
+       @item.description = ''
+       @item.valid?
+       expect(@item.errors.full_messages).to include "Description can't be blank"
+      end
+
+      it '商品説明が1000字以上では出品できない' do
+        @item.description = 'a' * 1001
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Description is too long (maximum is 1000 characters)"
+      end
+
+      it 'カテゴリーが未選択では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+
+      it '商品の状態が未選択では出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Condition can't be blank"
+      end
+
+      it '配送料の負担が未選択では出品できない' do
+        @item.shipping_cost_burden_id = 1
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Shipping cost burden can't be blank"
+      end
+
+      it '発送元地域が未選択では出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+      end
+
+      it '発送までの日数が未選択では出品できない' do
+        @item.shipping_day_id = 1
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Shipping day can't be blank"
+      end
+
+      it '価格が空では出品できない' do
+        @item.price = ''
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
+
+      it '価格が300円未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
+      end
+
+      it '価格が9,999,999円以上では出品できない' do
+        @item.price = 10000000
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
+      end
+
+      it '価格が半角数字以外では保存されない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      
+        @item.price = 'abcde'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+
+      it '商品画像がないと出品できない' do
+        @item.image = nil
+        @item.valid?
+       expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+
+      it 'ユーザーが存在しないと商品を出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "User must exist"
+      end
+    end
+  end
 end
