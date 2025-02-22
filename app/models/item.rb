6e53c6488e5ee6a05b8b9b3f6541acd4bdac21a5
kazu833 <tomsfashion833@gmail.com>
2025-02-22 22:59:33 +0900

    プルリクエスト　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,5 @@ class Item < ApplicationRecord
   validates :shipping_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true, format: { with: /\A\d+\z/, message: '半角数字のみを入力してください' },
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :user, presence: true
   validates :image, presence: true
 end
--- app/models/shipping_cost_burden.rb
+++ app/models/shipping_cost_burden.rb
@@ -6,5 +6,5 @@ class ShippingCostBurden < ActiveHash::Base
   ]
 
   include ActiveHash::Associations
-  has_many :articles
+  has_many :items
 end
