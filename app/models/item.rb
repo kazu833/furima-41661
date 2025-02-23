6265b324ef02d4c81f2bf80d73046ec3f8e0bc28
kazu833 <tomsfashion833@gmail.com>
2025-02-23 18:08:57 +0900

    アソシエーション　修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :shipping_cost_burden
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping_day
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost_burden
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   belongs_to :user
   has_one_attached :image
