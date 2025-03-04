17a2b213af43e8a3a06e0d4e0739ef478985dde9
kazu833 <tomsfashion833@gmail.com>
2025-03-04 19:35:56 +0900

    User遷移の設定

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,5 @@
 class RecordsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
   before_action :redirect_if_sold, only: [:index, :create]
 
